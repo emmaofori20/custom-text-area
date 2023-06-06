bdb45720a84acf069186f600feb24b9da1153ef7
Emmanuel Ofori <emmanuelofori2638@gmail.com>
2023-06-06 16:37:10 +0000

    styling component

--- src/app/Components/third-custom-text-area/third-custom-text-area.component.ts
+++ src/app/Components/third-custom-text-area/third-custom-text-area.component.ts
@@ -1,10 +1,17 @@
-import { AfterViewInit, Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-third-custom-text-area',
   templateUrl: './third-custom-text-area.component.html',
-  styleUrls: ['./third-custom-text-area.component.scss']
+  styleUrls: ['./third-custom-text-area.component.scss'],
 })
 export class ThirdCustomTextAreaComponent implements AfterViewInit {
   @Input() rows!: number;
@@ -14,36 +21,39 @@ export class ThirdCustomTextAreaComponent implements AfterViewInit {
   @Input() Autosize!: boolean;
   @Input() MinRow!: number;
   @Input() MaxRow!: number;
+  @Input() customLabelClass: string | undefined; // style label using tailwind
+  @Input() customTextAreaClass: string | undefined; // style label using tailwind
   @Input() customStyleLabel: { [key: string]: string } | undefined;
   @Input() customStyleTextArea: { [key: string]: string } | undefined;
   @Input() control = new FormControl();
 
   calculatedMaxHeight!: number;
-  @ViewChild('textareaElement') textareaElement!: ElementRef<HTMLTextAreaElement>;
+  @ViewChild('textareaElement')
+  textareaElement!: ElementRef<HTMLTextAreaElement>;
 
-  constructor(private renderer: Renderer2) {
-
-
-  }
+  constructor(private renderer: Renderer2) {}
 
   ngAfterViewInit() {
     this.autogrowTextarea();
     const { nativeElement } = this.textareaElement;
-    this.calculatedMaxHeight = (nativeElement.scrollHeight * this.MaxRow)- 80;
+    this.calculatedMaxHeight = nativeElement.scrollHeight * this.MaxRow - 120;
   }
 
-
-   autogrowTextarea() {
+  autogrowTextarea() {
     const { nativeElement } = this.textareaElement;
 
-    if(this.Autosize){
+    if (this.Autosize) {
       this.renderer.setStyle(nativeElement, 'height', 'auto');
-      this.renderer.setStyle(nativeElement, 'height', nativeElement.scrollHeight + 'px');
+      this.renderer.setStyle(
+        nativeElement,
+        'height',
+        nativeElement.scrollHeight + 'px'
+      );
 
       // Calculate the max number of rows the textarea can have
 
-      console.log("scrollHeight", nativeElement.scrollHeight)
-      console.log("maxheiht", this.calculatedMaxHeight)
+      console.log('scrollHeight', nativeElement.scrollHeight);
+      console.log('maxheiht', this.calculatedMaxHeight);
       // Check if the textarea height exceeds the max height
       if (nativeElement.scrollHeight > this.calculatedMaxHeight) {
         nativeElement.style.overflowY = 'scroll'; // Enable vertical scrolling
@@ -51,20 +61,16 @@ export class ThirdCustomTextAreaComponent implements AfterViewInit {
       } else {
         nativeElement.style.overflowY = 'hidden'; // Disable vertical scrolling
       }
-    }else{
-      console.log("dont auto size");
-
-
+    } else {
+      console.log('dont auto size');
     }
-
   }
 
-  getRows(){
-    if(this.Autosize){
-      return this.MinRow
-    }else{
-      return this.rows
+  getRows() {
+    if (this.Autosize) {
+      return this.MinRow;
+    } else {
+      return this.rows;
     }
   }
 }
-
